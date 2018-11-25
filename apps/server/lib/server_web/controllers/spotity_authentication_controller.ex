15080df01cd66ece32c0f18e09bdf848f2f71411
Zach Banducci <zbducci@gmail.com>
2018-11-25 15:36:41 -0600

    Added basic authentication and authorization functions required by Spotify

--- apps/server/lib/server_web/controllers/spotity_authentication_controller.ex
+++ apps/server/lib/server_web/controllers/spotity_authentication_controller.ex
@@ -1,2 +1,11 @@
 defmodule ServerWeb.SpotifyAuthController do
+  use ServerWeb, :controller
+
+  def authorize(conn, _params) do
+    redirect conn, external: Spotify.Authorization.url
+  end
+
+  def authenticate(conn, params) do
+    Spotify.Authentication.authenticate(conn, params)
+  end
 end
