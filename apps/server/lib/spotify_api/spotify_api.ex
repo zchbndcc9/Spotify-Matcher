24130f043a307b00af53a052346d4a6af727161a
Zach Banducci <zbducci@gmail.com>
2018-11-25 19:30:57 -0600

    Added basic pipe to raise flags if user is either authenticated or new

--- apps/server/lib/spotify_api/spotify_api.ex
+++ apps/server/lib/spotify_api/spotify_api.ex
@@ -1,2 +1,19 @@
-defmodule Server.SpotifyAPI do
+defmodule Server.SpotifyAPI.RaiseFlags do
+  import Plug.Conn
+
+  def init(default), do: default
+
+  def raise_token_flags(conn) do
+    assign(conn, :tokens_present?,  Spotify.Authentication.tokens_present?(conn))
+  end
+
+  def raise_authentication_flags(conn) do
+    assign(conn, :authenticated?, Spotify.Authentication.authenticated?(conn))
+  end
+
+  def call(conn, _params) do
+    conn
+    |> raise_token_flags
+    |> raise_authentication_flags
+  end
 end
