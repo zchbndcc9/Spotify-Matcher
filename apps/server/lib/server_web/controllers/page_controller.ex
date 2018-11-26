413abd2db5909b562f0f726c14f62570f7c62f71
Zach Banducci <zbducci@gmail.com>
2018-11-25 19:31:49 -0600

    Imported API plug & added conditional rendering/redirects based on flags

--- apps/server/lib/server_web/controllers/page_controller.ex
+++ apps/server/lib/server_web/controllers/page_controller.ex
@@ -1,9 +1,28 @@
 defmodule ServerWeb.PageController do
   use ServerWeb, :controller
+  plug Server.SpotifyAPI.RaiseFlags
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conditions = %{
+      tokens?: conn.assigns[:tokens_present?],
+      authenticated?: conn.assigns[:authenticated?]
+    }
+
+    determine_render(conn, conditions)
   end
 
-  
+  def determine_render(conn, %{tokens?: false}) do
+    conn
+    |> render("new_user.html")
+  end
+
+  def determine_render(conn, %{authenticated?: false}) do
+    conn
+    |> redirect(external: Spotify.Authorization.url)
+  end
+
+  def determine_render(conn, _conditions) do
+    conn
+    |> render("index.html")
+  end
 end
