c20c674e3637a9583c0f521d7210117c5128b87b
Zach Banducci <zbducci@gmail.com>
2018-11-25 20:14:51 -0600

    Added authorization handler to auth controller

--- apps/server/lib/server_web/controllers/spotity_authentication_controller.ex
+++ apps/server/lib/server_web/controllers/spotity_authentication_controller.ex
@@ -1,6 +1,11 @@
 defmodule ServerWeb.SpotifyAuthController do
   use ServerWeb, :controller
 
+  def authorize(conn, _params) do
+    conn
+    |> redirect(external: Spotify.Authorization.url)
+  end
+
   def authenticate(conn, params) do
     case Spotify.Authentication.authenticate(conn, params) do
       {:ok, conn} -> redirect conn, to: "/"
