9dbc415f46f094d465f83a1a7927d3498020afc2
Zach Banducci <zbducci@gmail.com>
2018-11-26 20:14:34 -0600

    Fixed a naming issue with the Artist Manager

--- apps/server/lib/server_web/channels/user_socket.ex
+++ apps/server/lib/server_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ServerWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  channel "app:*", ServerWeb.ArtistMangerChannel
+  channel "app:*", ServerWeb.ArtistManagerChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
