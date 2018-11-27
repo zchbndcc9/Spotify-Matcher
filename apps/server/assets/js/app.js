f2e4b547d7bb7d95743ee11209166996fb262227
Zach Banducci <zbducci@gmail.com>
2018-11-26 20:11:24 -0600

    Merged socket.js into app.js & implemented jquery fxnality for artist

--- apps/server/assets/js/app.js
+++ apps/server/assets/js/app.js
@@ -1,2 +1,21 @@
 import "phoenix_html"
-import socket from "./socket"
+import "jquery"
+
+import { Channel, Socket } from "phoenix"
+
+let addArtist = (artist) => {
+  channel.push("add", {name: artist})
+}
+
+$("div.artist-card").click(function(){
+  addArtist(this.textContent.trim())
+})
+
+let socket = new Socket("/socket", { params: { token: window.userToken } })
+
+socket.connect()
+
+let channel = socket.channel("app:artists", {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
