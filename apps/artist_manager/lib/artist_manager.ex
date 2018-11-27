57f082abd06e2285d028a1a32c6941cfb4ca722b
Zach Banducci <zbducci@gmail.com>
2018-11-27 12:50:00 -0600

    Added Librarian to manage artist library

--- apps/artist_manager/lib/artist_manager.ex
+++
@@ -1,9 +0,0 @@
-defmodule ArtistManager do
-  defdelegate add(artist), to: ArtistManager.Store
-  defdelegate get_all(), to: ArtistManager.Store
-  defdelegate add_and_get(artist), to: ArtistManager.Store
-  defdelegate contains?(artist), to: ArtistManager.Store
-  defdelegate update_similar_artists(artist, similar_artists), to: ArtistManager.Store
-  defdelegate get_similar_artists(artist), to: ArtistManager.Store
-  defdelegate similar_artists?(artist), to: ArtistManager.Store
-end
