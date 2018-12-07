4e42e735a5220801cdada16bd444ce7a959ccfd7
Zach Banducci <zbducci@gmail.com>
2018-12-07 14:04:40 -0600

    Added more styling to artist card

--- apps/server/assets/js/components/artist.jsx
+++ apps/server/assets/js/components/artist.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+const card = {
+  height: "3em",
+  cursor: "pointer"
+}
+
+const cardImg = {
+  borderRadius: "50%",
+  width: "3em",
+  height: "3em"
+}
+
 export const Artist = ({name, id, img}) => (
-  <div>
-    <img src={img.url} alt={img.url}/>
-    <h1>{name}</h1>
+  <div class="card" style={card}>
+    <img src={img.url} alt={img.url} style={cardImg}/>
+    <h6>{name}</h6>
   </div>
 )
