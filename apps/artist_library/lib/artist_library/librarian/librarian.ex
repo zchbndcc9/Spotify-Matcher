eef0fc56ed8a0becf2c4563e92e701de5e8af2af
Zach Banducci <zbducci@gmail.com>
2018-12-07 20:33:26 -0600

    Fixed some compile errors

--- apps/artist_library/lib/artist_library/librarian/librarian.ex
+++ apps/artist_library/lib/artist_library/librarian/librarian.ex
@@ -6,20 +6,6 @@ defmodule ArtistLibrary.Librarian do
 
   def checkin_artist(artist) do
     artist
-    |> ArtistLibrary.Library.contains?
-    |> update_library(artist)
-  end
-
-  def checkout_sim_artists(artist) do
-    artist
-    |> Library.add_and_get()
-    |> Library.update_similar_artists(similar_artists)
-  end
-
-  def checkout_sim_artist(artist) do
-    artist
-    |> checkout_sim_artists
-    |> Enum.random
   end
 
   def checkout_sim_artists(artist) do
