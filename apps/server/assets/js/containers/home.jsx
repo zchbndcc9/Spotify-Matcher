f4e0952b2bbb62f9bbfce792314b26896613e4ee
Zach Banducci <zbducci@gmail.com>
2018-12-08 23:15:29 -0600

    Added form for playlist creation

--- apps/server/assets/js/containers/home.jsx
+++ apps/server/assets/js/containers/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import ArtistList from '../components/artist-list';
+import ArtistList from '../components/artist-list'
 
 class Home extends React.Component {
   constructor(props) {
@@ -39,8 +39,34 @@ class Home extends React.Component {
   }
 
   render() {
+    const message = this.state.pickedArtists.length < 5 ? "Please pick at least 5 artists" : "Good to go!"
     return(
-      <ArtistList artists={this.state.artists} pickArtist={(id) => this.pickArtist(id)} />
+      <div className="row">
+        <h2>{message}</h2>
+        <ArtistList artists={this.state.artists} pickArtist={(id) => this.pickArtist(id)} />
+        <form className="form">
+          <div className="form-group">
+            <label htmlFor="title">Playlist Title</label>
+            <input id="title" className="form-control" type="text" placeholder="My cool playlist" required/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="num-songs">Number of Songs</label>
+            <div className="form-check form-check-inline">
+              <label className="form-check-label" htmlFor="input1">10</label>
+              <input className="form-check-input" type="radio" name="num-songs" id="input1" value="10" required/>
+            </div>
+            <div className="form-check form-check-inline">
+              <label className="form-check-label" htmlFor="input2">30</label>
+              <input className="form-check-input" type="radio" name="num-songs" id="input2" value="30" required/>
+            </div>
+            <div className="form-check form-check-inline">
+              <label className="form-check-label" htmlFor="input3">50</label>
+              <input className="form-check-input" type="radio" name="num-songs" id="input3" value="50" required/>
+            </div>
+          </div>
+          <button className="btn btn-success" disabled={this.state.pickedArtists.length < 5}>Create playlist</button>
+        </form>
+      </div>
     )
   }
 }
