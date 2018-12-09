b071da4f802b8c76d8408c9b868c6a024f2fc7ed
Zach Banducci <zbducci@gmail.com>
2018-12-09 13:06:17 -0600

    Added functionality to clear store if user restarts application

--- apps/artist_manager/lib/artist_manager/store_supervisor.ex
+++ apps/artist_manager/lib/artist_manager/store_supervisor.ex
@@ -14,4 +14,13 @@ defmodule ArtistManager.StoreSupervisor do
     spec = {ArtistManager.Store, user_id}
     DynamicSupervisor.start_child(__MODULE__, spec)
   end
+
+  def restart_store(user_id) do
+    case Registry.lookup(:store_registry, user_id) do
+      [{pid, _}] -> DynamicSupervisor.terminate_child(__MODULE__, pid)
+      _ -> nil
+    end
+    
+    create_store(user_id)
+  end
 end
